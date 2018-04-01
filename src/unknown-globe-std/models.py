3214957457617ebf06aab0aa380c6587065a12c5
Nicholas Hall <theologue@gmail.com>
2018-04-01 20:06:13 +0100

    Refactoring

--- src/unknown-globe-std/models.py
+++ src/unknown-globe-std/models.py
@@ -16,27 +16,29 @@ class PostHelper(object):
     """ Helps to format post data from datastore into client-friendly structure.
   
     Attributes:
-        id: A string URL-safe post ID.
-        data: An object containing the post body data.
+        post: An object containing formatted post data.
     """
   
     def __init__(self, post):
-        self.id = post.key.urlsafe()
         post_dict = post.to_dict()
         post_date = post_dict.get('date', None)
-        self.data = {
-            'date': int(time.mktime(post_date.timetuple())) if post_date else None,
-            'date_string': str(post_date) if post_date else None,
-            'title': post_dict.get('title', None),
-            'image': post_dict.get('image', None),
-            'category': self._category_enum_converter(post_dict.get('category', 0)),
-            'content': {
-                'en': post_dict.get('en', None),
-                'pl': post_dict.get('pl', None),
-                'pt': post_dict.get('pt', None)
+        
+        self.post = {
+            'id': post.key.urlsafe(),
+            'data': {
+                'date': int(time.mktime(post_date.timetuple())) if post_date else None,
+                'date_string': str(post_date) if post_date else None,
+                'title': post_dict.get('title', None),
+                'image': post_dict.get('image', None),
+                'category': self._category_enum_converter(post_dict.get('category', 0)),
+                'content': {
+                    'en': post_dict.get('en', None),
+                    'pl': post_dict.get('pl', None),
+                    'pt': post_dict.get('pt', None)
+                }
             }
         }
-    
+        
     def _category_enum_converter(self, category_id):
         categories = {
             0: 'Unclassified',
@@ -44,10 +46,7 @@ class PostHelper(object):
             2: 'Travel'
         }
     
-        return categories.get(category_id, None)
+        return categories.get(category_id, "Unknown")
     
     def get_post(self):
-        return {
-            'id': self.id,
-            'data': self.data
-        }
+        return self.post
